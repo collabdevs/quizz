719f6b1eaf2ebab83ee4a3b220a890bc83f9948d
Daniel Medeiros <danielmmf@gmail.com>
2017-06-12 03:12:22 -0300

    geolocation

--- index.php
+++ index.php
@@ -93,6 +93,9 @@
     <script src="js/jquery.cookie.js"> </script>
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/front.js"></script>
+     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMJEuu01oa6tT1LGdrq17m9LQs01K_Euc"
+  type="text/javascript"></script>
+        <!--script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMJEuu01oa6tT1LGdrq17m9LQs01K_Euc"></script>
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID.-->
     <!---->
     <script>
@@ -153,6 +156,10 @@
           .when('/instrucoes', {
             templateUrl:'views/instrucoes.html'
           })
+           .when('/registrar', {
+            controller:'RegistrarController as Registrar',
+            templateUrl:'views/registrar.html'
+          })
           .when('/edit/:projectId', {
             controller:'EditProjectController as editProject',
             templateUrl:'detail.html',
@@ -166,6 +173,69 @@
           .otherwise({
             redirectTo:'/'
           });
+      })
+
+      .controller('RegistrarController', function() {
+
+
+        var showPosition = function(position) {
+
+
+        //Paulista var location = new google.maps.LatLng(-23.5632103, -46.6542503);
+         var location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            var mapCanvas = document.getElementById('map');
+            var mapOptions = {
+                center: location,
+                zoom: 16,
+                panControl: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+            var map = new google.maps.Map(mapCanvas, mapOptions);
+
+            var markerImage = 'img/marker.png';
+
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map,
+                icon: markerImage
+            });
+
+            var contentString = '<div class="info-window">' +
+                    '<h3>Info Window Content</h3>' +
+                    '<div class="info-content">' +
+                    '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                    '</div>' +
+                    '</div>';
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString,
+                maxWidth: 400
+            });
+
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+
+            var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+
+            map.set('styles', styles);
+      }
+
+
+         var initMap = function() {
+
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+
+            } else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+
+        }
+
+        google.maps.event.addDomListener(window, 'load', initMap);
+
       })
        
       .controller('ProjectListController', function(projects) {
